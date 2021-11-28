aade060c1f29c763969695c11f3247710388ae9c
Jeon <abc8339265@gmail.com>
2021-11-28 16:18:59 +0900

    update loginPage and registerPage

--- src/components/auth/AuthForm.js
+++ src/components/auth/AuthForm.js
@@ -50,10 +50,20 @@ const Footer = styled.div`
   }
 `;
 
-const AuthForm = () => {
+const textMap = {
+  login: '로그인',
+  register: '회원가입'
+}
+
+const ButtonWithMarginTop = styled(Button)`
+  margin-top: 1rem;
+`
+
+const AuthForm = ({ type }) => {
+  const text = textMap[type];
   return (
     <AuthFormBlock>
-      <h3>로그인</h3>
+      <h3>{text}</h3>
       <form>
         <StyledInput autoComplete={"username"} name={"username"} placeholder={"아이디"}/>
         <StyledInput
@@ -62,10 +72,21 @@ const AuthForm = () => {
           placeholder={"비밀번호"}
           type={"password"}
         />
-        <Button>로그인</Button>
+        {type === 'register' && (
+          <StyledInput
+            autoComplete={"new-password"}
+            name={"passwordConfirm"}
+            placeholder={"비밀번호 확인"}
+            type={"password"}
+            />
+        )}
+        <ButtonWithMarginTop cyan fullWidth>로그인</ButtonWithMarginTop>
       </form>
       <Footer>
-        <Link to={"/register"}>회원가입</Link>
+        {type === 'login' ?
+          (<Link to={"/register"}>회원가입</Link>) :
+          (<Link to={"/login"}>로그인</Link>)
+        }
       </Footer>
     </AuthFormBlock>
   );
