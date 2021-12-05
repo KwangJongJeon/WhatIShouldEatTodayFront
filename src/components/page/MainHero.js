756abc33fa87e7efac0ebbf9cb44a97cc369d6cd
Jeon <abc8339265@gmail.com>
2021-12-05 19:09:30 +0900

    # add image on Homepage

--- src/components/page/MainHero.js
+++ src/components/page/MainHero.js
@@ -1,10 +1,12 @@
+import mainImage from '../../images/mainImage.jpg'
+
 const MainHero = () => {
   return (
     <div className={'container col-xxl-8 px-4 py-5'}>
       <div className={'row flex-lg-row-reverse align-items-center g-5 py-5'}>
         <div class={'col-10 col-sm-8 col-lg-6'}>
           <img
-            src={'#'}
+            src={mainImage}
             className={'d-block mx-lg-auth img-fluid'}
             alt={'오늘 뭐먹지 메인 이미지'}
             width={'700'}
