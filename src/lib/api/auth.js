88a0fdf0623817582ac14caf6daba0cd5fec203c
Jeon <abc8339265@gmail.com>
2022-03-24 15:18:54 +0900

    # add 'withCredential' Property in client.js

--- src/lib/api/auth.js
+++ src/lib/api/auth.js
@@ -5,13 +5,14 @@ export const login = ({ memberEmail, memberPw }) => {
   const formData = new FormData();
   formData.append('memberEmail', memberEmail);
   formData.append('memberPw', memberPw);
-  return client.post('/api/auth/login', formData, {withCredentials: true});
+  return client.post('/api/auth/login', formData);
 
 }
 
 // 회원가입
 export const register = ({ memberEmail, memberPw, memberName, phoneNumber }) => {
-  return client.post('/api/auth/register', { memberEmail, memberPw, memberName, phoneNumber}, {withCredentials: true});
+  return client.post('/api/auth/register', { memberEmail, memberPw, memberName, phoneNumber},
+    {withCredentials: true});
 }
 
 // 로그인 상태 확인
@@ -25,7 +26,6 @@ export const getUserInfo = () => client.get('/api/auth/getMember');
 
 // 사용자 정보 수정
 export const editUserInfo = ({ memberId, memberEmail, memberName, nickName, phoneNumber}) => {
-  return client.post('/api/auth/editMember', {memberId, memberEmail, memberName, nickName, phoneNumber},
-    {withCredentials: true});
+  return client.post('/api/auth/editMember', {memberId, memberEmail, memberName, nickName, phoneNumber});
 }
 
