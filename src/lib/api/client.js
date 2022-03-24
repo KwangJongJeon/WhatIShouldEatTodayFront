3a4b0d4d4b0ca508ef6c8dd4f4725c166aa64071
Jeon <abc8339265@gmail.com>
2022-03-24 15:48:41 +0900

    # edit gitignore

--- src/lib/api/client.js
+++ src/lib/api/client.js
@@ -2,7 +2,6 @@ import axios from 'axios'
 
 const client = axios.create({
   // 서버에 따른 URL을 추가해줘야합니다.
-  baseURL: 'http://localhost:8080',
-  withCredentials: true
+  baseURL: 'http://localhost:8080'
 });
 export default client;
